0b007e6fb239e1001a78eea6990e82ff48c93d41
FadeIn64 <danik6052@gmail.com>
2024-05-11 11:45:19 +0400

    add auth\{login} endpoint for admins

--- simple-sso/src/main/java/com/lowfi/simplesso/controllers/AuthController.java
+++ simple-sso/src/main/java/com/lowfi/simplesso/controllers/AuthController.java
@@ -1,14 +1,35 @@
 package com.lowfi.simplesso.controllers;
 
+import com.lowfi.simplesso.services.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class AuthController {
+    @Autowired
+    UserDetailsServiceImpl service;
     @GetMapping("/auth")
-    public ResponseEntity test( Authentication authentication){
+    public ResponseEntity auth(Authentication authentication){
         return ResponseEntity.ok(authentication.getPrincipal());
+    }
+
+    @PreAuthorize("hasAuthority('admin')")
+    @GetMapping("/auth/{login}")
+    public ResponseEntity auth(@PathVariable String login){
+        try {
+            return ResponseEntity.ok(service.loadUserByUsername(login));
+        }
+        catch (UsernameNotFoundException e){
+            return new ResponseEntity<>(HttpStatusCode.valueOf(401));
+        }
     }
 }
