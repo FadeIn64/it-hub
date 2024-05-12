2b19dcf0e275f232b032aec8547e2ecaf84010ad
FadeIn64 <danik6052@gmail.com>
2024-05-12 16:03:35 +0400

    crate Competition controller

--- competition-service/src/main/java/com/lowfi/competitionservice/CompetitionServiceApplication.java
+++ competition-service/src/main/java/com/lowfi/competitionservice/CompetitionServiceApplication.java
@@ -1,5 +1,6 @@
 package com.lowfi.competitionservice;
 
+import com.lowfi.competitionservice.repos.CompetitionRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CompetitionServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CompetitionServiceApplication.class, args);
+
+       var ctx = SpringApplication.run(CompetitionServiceApplication.class, args);
+       var repo = ctx.getBean(CompetitionRepo.class);
+        System.out.println(repo.findByIdWithThemes(1));
     }
 
 }
