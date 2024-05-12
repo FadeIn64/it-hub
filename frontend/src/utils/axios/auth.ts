04490fc3002b50dbcba23dcbbcd742723765bd9f
iliyagoga <100951861+iliyagoga@users.noreply.github.com>
2024-05-13 00:08:11 +0400

    4.0

--- frontend/src/utils/axios/auth.ts
+++ frontend/src/utils/axios/auth.ts
@@ -5,7 +5,11 @@ export class Requests{
     async auth(login: string, pass: string){
         const base = Base64.encode(login+":"+pass)
         try {
-            const res =  await axios.get(api.host+api.auth, {headers: {Authorization: 'Basic '+base}})
+
+            const res =  await axios.get(api.host+api.auth, {auth:{
+                username: 'admin',
+                password: 'admin'
+            }})
             console.log(res)
         } catch (error) {
             console.log(error)
