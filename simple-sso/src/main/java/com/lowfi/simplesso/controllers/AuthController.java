6a68a1c273fded7095feb749921c49143ce92766
FadeIn64 <danik6052@gmail.com>
2024-05-13 00:30:53 +0400

    correct 8 support for competition-service

--- simple-sso/src/main/java/com/lowfi/simplesso/controllers/AuthController.java
+++ simple-sso/src/main/java/com/lowfi/simplesso/controllers/AuthController.java
@@ -10,6 +10,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Method;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/sso")
