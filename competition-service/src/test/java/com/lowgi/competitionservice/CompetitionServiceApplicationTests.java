280fa5c8073232cd163d0c9f518ea79aae597d1f
FadeIn64 <danik6052@gmail.com>
2024-05-12 15:19:16 +0400

    init competition service

---
+++ competition-service/src/test/java/com/lowgi/competitionservice/CompetitionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lowgi.competitionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompetitionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
